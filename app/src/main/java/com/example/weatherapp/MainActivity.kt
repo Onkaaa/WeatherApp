929a50c60f4476e41a40fbd3fc81dd20ffe80894
Onkaaa <165186519+Onkaaa@users.noreply.github.com>
2024-06-10 14:37:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Identity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.annotation.ReturnThis
+import com.google.android.filament.Stream
+import java.nio.file.Files.find
+import java.nio.file.Path
+import java.util.stream.Stream
+
+private infix fun Nothing?.View(by: Nothing?) {view
+
+}
+
+private infix fun Any.Identity(btnAdd: Number): Button {
+
+}
+
+class MainActivity : AppCompatActivity() {View
+    private fun Identity(btnSubtraction: Any): Identity {
+        TODO("Not yet implemented")
+    }
+
+    private fun Button(z: Button): Button {
+
+    }
+
+    private fun find(): Stream<Path>? {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        lateinit var add:Button
+        lateinit var subtraction:Button
+        lateinit var multiplication:Button
+        lateinit var Division:Button
+        val By = null
+        Button(add) = find() View By Identity(R.id.btn_add)
+        Button(subtraction) = find() View By Identity(R.id.btn_add)
+        val Find = null
+        Button(multiplication) = Find View By Identity(R.id.btn_add)
+
+        this.findViewById(R.id.et_a).also {
+            val it = null
+            EditText(add) = it
+        }
+        findViewById((setResult)).also {
+            val it = null
+            Result = it
+        }
+        Button(add) =setonclicklistener(this)
+        Button(subtraction) =setonclicklistener(this)
+        Button(multiplication) =setonclicklistener(this)
+        Button() divide=setonclicklistener(this)
+        val etA
+        var.a=etA.text.tostring()to Double() var = setResult(0)
+
+        val v = null
+        val button = null
+        when(v.)R.id button=add
+
+
+
+
+
+
+    }
+}
